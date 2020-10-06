ab05231bb96151aa7d9a98f832e0323bf059482e
tfunk2 <tyler.j.funk2@gmail.com>
2020-10-06 02:11:46 -0600

    Added state, and ability to set a winning number, and view previous 20 winning numbers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [chipCount, useChipCount] = useState(1000);
+  const [currentBetValue, setCurrentBetValue] = useState(null);
+  const [winningNumber, setWinningNumber] = useState(null);
+  const [previousTwenty, setPreviousTwenty] = useState([]);
+  const [straightUps, setStraightUps] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0
+  ]);
+  const [splits, setSplits] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0
+  ]);
+  const [streets, setStreets] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]);
+  const [corners, setCorners] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]);
+  const [basket, setBasket] = useState(0);
+  const [columns, setColumns] = useState(0, 0, 0);
+  const [dozens, setDozens] = useState(0, 0, 0);
+  const [redBlack, setRedBlack] = useState(null);
+  const [oddEven, setOddEven] = useState(null);
+  const [highLow, setHighLow] = useState(null);
+
+  const spinTheWheel = () => {
+    const wheelNumbers = [
+      "0", "1", "2", "3", "4", "5", "6", "7", "8",
+      "9", "10", "11", "12", "13", "14", "15", "16",
+      "17", "18", "19", "20", "21", "22", "23", "24",
+      "25", "26", "27", "28", "29", "30", "31", "32",
+      "33", "34", "35", "36", "00"
+    ];
+    let randomIndex = Math.floor(Math.random() * Math.floor(38));
+    let randomWinner = wheelNumbers[randomIndex];
+    setWinningNumber(randomWinner);
+
+    let currentTwenty = [...previousTwenty];
+
+    if(currentTwenty.length < 20) {
+      setPreviousTwenty([...previousTwenty, randomWinner]);
+    } else {
+      currentTwenty.unshift(randomWinner);
+      currentTwenty.pop();
+      setPreviousTwenty(currentTwenty);
+    };
+  };
+
+  const wheelHistory = previousTwenty.map(winningNum => {
+    return <p>{winningNum}</p>
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header" onClick={spinTheWheel}>
+        Spin the Wheel - {winningNumber}
       </header>
+      {wheelHistory}
     </div>
   );
-}
+};
 
 export default App;
